2294ca5d3c7b410741e648a35f05aa25b84041d4
tpeck428 <toripeck73@gmail.com>
2024-05-05 20:54:03 -0400

    switched up code and can create events and save them to calendar

--- frontend/capstone-project/src/components/plannerComponents/addEventModal.jsx
+++ frontend/capstone-project/src/components/plannerComponents/addEventModal.jsx
@@ -1,52 +1,54 @@
-// import React from "react";
-// import ReactModal from "react-modal";
-// import { useState } from "react";
-// import Datetime from 'react-datetime';
+import React from "react";
+import { useState } from "react";
+import Datetime from 'react-datetime';
+import Modal from "react-modal";
 
-// export default function ({ isOpen, onClose, onEventAdded }) {
+function AddEventModal({ isOpen, onClose, onEventAdded }) {
 
-//     const [title, setTitle] = useState("");
-//     const [content, setContent] = useState("");
-//     const [start, setStart] = useState(new Date());
-//     const [end, setEnd] = useState(new Date());
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
 
-//     const onSubmit = (e) => {
-//         e.preventDefault();
+    const onSubmit = (e) => {
+        e.preventDefault();
 
-//         onEventAdded({
-//             title,
-//             content,
-//             start,
-//             end,
-//         })
-//         onClose();
-//     }
+        onEventAdded({
+            title,
+            content,
+            start,
+            end,
+        })
+        onClose();
+    }
 
 
-//     return (
-//         <Modal isOpen={isOpen} onRequestClose={onClose}>
-//             <form onSubmit={onSubmit}>
-//                 <input
-//                     placeholder="Title"
-//                     value={title}
-//                     onChange={e => setTitle(e.target.value)}
-//                 />
-//                 <input
-//                     placeholder="Content"
-//                     value={content}
-//                     onChange={e => setContent(e.target.value)}
-//                 />
-//                 <div>
-//                     <label>Start Date</label>
-//                     <Datetime value={start} onChange={date => setStart(date)} />
-//                 </div>
-//                 <div>
-//                     <label>End Date</label>
-//                     <Datetime value={end} onChange={date => setEnd(date)} />
-//                 </div>
+    return (
+        <Modal isOpen={isOpen} onRequestClose={onClose}>
+            <form onSubmit={onSubmit}>
+                <input
+                    placeholder="Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <input
+                    placeholder="Content"
+                    value={content}
+                    onChange={e => setContent(e.target.value)}
+                />
+                <div>
+                    <label>Start Date</label>
+                    <Datetime value={start} onChange={date => setStart(date)} />
+                </div>
+                <div>
+                    <label>End Date</label>
+                    <Datetime value={end} onChange={date => setEnd(date)} />
+                </div>
 
-//                 <button onClick={()=> setModalOpen(true)}>Add Event</button>
-//             </form>
-//         </Modal>
-//     )
-// }
+                <button onClick={() => isOpen(true)}>Add Event</button>
+            </form>
+        </Modal>
+    )
+}
+
+export default AddEventModal;
