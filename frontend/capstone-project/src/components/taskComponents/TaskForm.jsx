8d97e187d0f6606c9df8f581a99e78b7ecd213fa
tpeck428 <toripeck73@gmail.com>
2024-05-06 14:00:44 -0400

    working handleSubmit function for adding task

--- frontend/capstone-project/src/components/taskComponents/TaskForm.jsx
+++ frontend/capstone-project/src/components/taskComponents/TaskForm.jsx
@@ -4,8 +4,13 @@ export default function TaskForm() {
 
 const [value, setValue] = useState("");
 
+const handleSubmit = ev => {
+    ev.preventDefault();
+    // console.log(value) / logs to console
+}
+
   return (
-    <form className='TaskForm'>
+    <form className='TaskForm' onSubmit={handleSubmit}>
         <input 
         type='text' 
         className='task-input' 
